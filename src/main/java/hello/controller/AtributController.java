4e9a7dde602984134ce1b957c09c27a29c5b6761
IlyaToporovskiy <lelikleti0@gmail.com>
2019-06-17 00:31:20 +0300

    4

--- src/main/java/hello/controller/AtributController.java
+++ src/main/java/hello/controller/AtributController.java
@@ -1,5 +1,9 @@
 package hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AtributController {
 
+
     @RequestMapping(method = RequestMethod.GET,value = "/response")
     public ModelAndView home()
     {
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(userDetails.getUsername().toString());
+
+
         return new ModelAndView("test");
     }
 }
--- src/main/java/hello/repository/PersonRepo.java
+++ src/main/java/hello/repository/PersonRepo.java
@@ -1,9 +1,10 @@
 package hello.repository;
 
+import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.ldap.core.LdapTemplate;
 
 import java.util.List;
-
+@EnableLdapRepositories
 public interface PersonRepo {
    void setLdapTemplate(LdapTemplate ldapTemplate);
    List<String> getAllPersonNames();
